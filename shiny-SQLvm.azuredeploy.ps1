8947fb6a397ebb03f0b6040015d95019d6b75a18
pyasuda <pyasuda@getadvantage.com>
2021-05-28 17:06:48 -0400

    Initial file changes to match AZ-TEST-DB3

--- shiny-SQLvm.azuredeploy.ps1
+++ shiny-SQLvm.azuredeploy.ps1
@@ -1,5 +1,5 @@
 # The vnet in the parameters file must be in the same resource group as the VM
 New-AzResourceGroupDeployment `
 -ResourceGroupName rg-AzureMigrateTest-mpn-001 `
--TemplateFile .\shiny-vm.azuredeploy.json `
--TemplateParameterFile .\shiny-vm.azuredeploy.parameters.json
+-TemplateFile .\shiny-SQLvm.azuredeploy.json `
+-TemplateParameterFile .\shiny-SQLvm.azuredeploy.parameters.json
